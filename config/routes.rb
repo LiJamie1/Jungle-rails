910b058ee3433e29d9f6f9d14786ce559999d58d
Jamie <lijamie19828@gmail.com>
2022-01-07 00:20:06 +0000

    update routes and session controller, create new sessions view

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,10 @@ Rails.application.routes.draw do
   end
 
   # Login and Authentication
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
