008059b5e979cfa4783fff4046922442cb10e0d2
Jamie <lijamie19828@gmail.com>
2022-01-11 02:10:34 +0000

    fix error remove bang from save

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -22,8 +22,8 @@ RSpec.describe Product, type: :model do
     it 'should not save when missing a name' do
       @product_params[:name] = nil
       @product = Product.new(@product_params)
-      @product.save!
-      
+      @product.save
+
       expect(@product.id).not_to be_present
       expect(@product.errors.full_messages).to include("Name can't be blank")  
     end
