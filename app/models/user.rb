295f755652834e42f949c6a2fc8219213e7cdfb3
Jamie <lijamie19828@gmail.com>
2022-01-11 06:02:31 +0000

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, presence: true
+  validates :password, confirmation: true
+  validates :password, length: { minimum: 8 }
+   
 end
