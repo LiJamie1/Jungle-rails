cbd998586850bd9eb91bc7114a1fa616f0558a73
Jamie <lijamie19828@gmail.com>
2022-01-11 04:22:14 +0000

    test 1, passwords don't match

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validation' do
+    before { 
+      @user_params = {
+        name: 'Jamie',
+        email: 'test@test.test',
+        password: 'password',
+        password_confirmation: 'password'
+      }
+    }
+
+    it 'should not save if passwords do not match' do
+      @user_params[:password_confirmation] = 'notPassword'
+      @user = User.create(@user_params)
+
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+  end
 end
